4e2836878d2a09624c360e24dca95f101f02341d
Juan Herrera <jkh2.omg@gmail.com>
2021-07-05 21:00:17 -0500

    Datos personales

--- SecondGithub.java
+++ SecondGithub.java
@@ -2,6 +2,11 @@ public class SecondGithub{
 
 
 public static void main(String [] args){
+String name="Juan";
+int age=21;
+
+System.out.println("Greetings");
+System.out.println("Mi nombre es "+name+" .Tengo "+age+" años");
 
 }
 
